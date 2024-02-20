536f669d591bd76f5f06d51065ef406202001494
Luis Vieira Relvas <up202108661@fe.up.pt>
2024-02-20 20:02:17 +0000

    Update MyScene.js

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -108,9 +108,9 @@ export class MyScene extends CGFscene {
 
     if(this.displayParallellogram) this.parallellogram.display();
 
-    // this.myTriangleSmall.display();
+    this.myTriangleSmall.display();
     
-    // this.myTriangleBig.display();
+    this.myTriangleBig.display();
 
     // ---- END Primitive drawing section
   }
