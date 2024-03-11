29e4daf8058e99dcce24ded6d0940a92dd617452
andrensenn <andrensenn@gmail.com>
2024-03-11 14:10:26 +0000

    prism finished

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -3,19 +3,15 @@ import {CGFobject} from '../lib/CGF.js';
  * MyPrism
  * @constructor
  * @param scene - Reference to MyScene object
+ * @param slices - number of divisions around the Y axis
+ * @param stacks - number of divisions along the Y axis
  */
+
 export class MyPrism extends CGFobject {
-	constructor(scene, slices, stacks)
-
-	{
-
-                super(scene);
-
-                this.slices = slices;
-
-                this.stacks = stacks;
-
-
+	constructor(scene, slices, stacks){
+        super(scene);
+        this.slices = slices;
+        this.stacks = stacks;
 		this.initBuffers();
 
 	}
@@ -42,9 +38,18 @@ export class MyPrism extends CGFobject {
                 let y = Math.sin((i+0.5)*increment);
                 let size = Math.sqrt(x*x + y*y);
 
-                this.vertices.push(x1, y1, incrementZ * j, x2, y2, incrementZ * j, x1, y1, incrementZ * (j + 1), x2, y2, incrementZ * (j + 1));
-                this.indices.push(index+2, index, index+1, index+1, index+3, index+2);
-                this.normals.push(x/size, y/size, 0, x/size, y/size, 0, x/size, y/size, 0, x/size, y/size, 0);
+                this.vertices.push(x1, y1, incrementZ * j); 
+                this.vertices.push(x2, y2, incrementZ * j);
+                this.vertices.push(x1, y1, incrementZ * (j + 1)); 
+                this.vertices.push(x2, y2, incrementZ * (j + 1));
+
+                this.indices.push(index+2, index, index+1);
+                this.indices.push(index+1, index+3, index+2);
+
+                this.normals.push(x/size, y/size, 0, x/size);
+                this.normals.push(y/size, 0, x/size, y/size);
+                this.normals.push(0, x/size, y/size, 0);
+                
                 index+=4;
             }
         }
@@ -53,4 +58,5 @@ export class MyPrism extends CGFobject {
     
 	}
 }
+
 
