9161b8cd07351e5bf1968d6f61c61146ed57139b
LuisRelvas <luisrelvas@netcabo.pt>
2024-03-13 18:59:55 +0000

    Finished TP3

--- tp3/MyCilinder.js
+++ tp3/MyCilinder.js
@@ -1,6 +1,6 @@
 import { CGFobject } from '../lib/CGF.js';
 /**
- * MyCilinder
+ * MyCylinder
  * @constructor
  * @param scene - Reference to MyScene object
  */
@@ -12,110 +12,82 @@ export class MyCilinder extends CGFobject {
         this.initBuffers();
     }
 
-
-    initBuffers(){
-
-        this.vertices = [];
-        this.indices = [];
-        this.normals = [];
-
-        let index = 0;
-        let increment = 2 * Math.PI / this.slices;
-        for (let i = 0 ; i < this.slices ; i++) {
-
-            let x1 = Math.cos(i*increment);
-            let y1 = Math.sin(i*increment);
-            let x2 = Math.cos((i+1)*increment);
-            let y2 = Math.sin((i+1)*increment);
-            
-            let incrementZ = 1 / this.stacks
-            for (let j = 0 ; j < this.stacks ; j++) {
-
-                let angle = 2 * Math.PI * i / this.slices;
-                let x = Math.cos(angle);
-                let y = Math.sin(angle);
-
-                this.vertices.push(x1, y1, incrementZ * j); 
-                this.vertices.push(x2, y2, incrementZ * j);
-                this.vertices.push(x1, y1, incrementZ * (j + 1)); 
-                this.vertices.push(x2, y2, incrementZ * (j + 1));
-
-                this.indices.push(index+2, index, index+1);
-                this.indices.push(index+1, index+3, index+2);
-
-                this.normals.push(x, y, 0); 
-                this.normals.push(0, 0, 0);
-                this.normals.push(x, y, 0);
-                this.normals.push(0, 0, 0);
-                
-                index+=4;
-            }
-        }
-		this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();
-
-        
-    }
-}
-/*
-
-    
     initBuffers() {
-
         this.vertices = [];
         this.indices = [];
         this.normals = [];
-        
-        for (let z = 0 ; z <= this.stacks ; z += 1) {
+    
+        this.addVerticesAndNormalsForStacks();
+        this.calculateVerticesNormalsAndIndicesForSlices();
+    
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+    
+    addVerticesAndNormalsForStacks() {
+        for (let z = 0; z <= this.stacks; z += 1) {
             this.vertices.push(1, 0, z / this.stacks);
             this.normals.push(1, 0, 0);
         }
-
-        for (let i = 1 ; i <= this.slices ; i++) {
-
-            let angle = 2 * Math.PI * i / this.slices;
-            let x = Math.cos(angle);
-            let y = Math.sin(angle);
-
-            let vector_size = Math.sqrt(x * x + y * y);
+    }
+    
+    calculateVerticesNormalsAndIndicesForSlices() {
+        for (let i = 1; i <= this.slices; i++) {
+            let {x, y, vector_size} = this.calculateVector(i);
+    
             if (i != this.slices) {    
                 this.vertices.push(x, y, 0);
                 this.normals.push(x / vector_size, y / vector_size, 0);
             }
-
-            for (let j = 1 ; j <= this.stacks ; j++) {
-                
-                if (i != this.slices) {
-
-                    let z = j / this.stacks;
-                    this.vertices.push(x, y, z);
-                    this.normals.push(x / vector_size, y / vector_size, 0);
-                    
-                    let points = this.vertices.length / 3;
-                    let indexC = points - 2;
-                    let indexD = points - 1;
-                    let indexB = indexD - (this.stacks + 1);
-                    let indexA = indexB - 1;
-                    this.indices.push(indexA, indexC, indexD, indexA, indexD, indexB);
-
-                } else {
-
-                    let points = this.vertices.length / 3;
-                
-                    let indexC = j - 1;
-                    let indexD = j;
-                    let indexB = points - this.stacks - 1 + j;
-                    let indexA = indexB - 1;
-                    this.indices.push(indexA, indexC, indexD, indexA, indexD, indexB);
-                }
+    
+            this.calculateVerticesNormalsAndIndicesForStacks(i, x, y, vector_size);
+        }
+    }
+    
+    calculateVector(i) {
+        let angle = 2 * Math.PI * i / this.slices;
+        let x = Math.cos(angle);
+        let y = Math.sin(angle);
+        let vector_size = Math.sqrt(x * x + y * y);
+        return {x, y, vector_size};
+    }
+    
+    calculateVerticesNormalsAndIndicesForStacks(i, x, y, vector_size) {
+        for (let j = 1; j <= this.stacks; j++) {
+            if (i != this.slices) {
+                this.addStackVertex(x, y, j, vector_size);
+                this.addIndices(j);
+            } else {
+                this.addFinalIndices(j);
             }
         }
-
-        this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();
     }
+    
+    addStackVertex(x, y, j, vector_size) {
+        let z = j / this.stacks;
+        this.vertices.push(x, y, z);
+        this.normals.push(x / vector_size, y / vector_size, 0);
+    }
+    
+    addIndices(j) {
+        let points = this.vertices.length / 3;
+        let indexC = points - 2;
+        let indexD = points - 1;
+        let indexB = indexD - (this.stacks + 1);
+        let indexA = indexB - 1;
+        this.indices.push(indexA, indexC, indexD, indexA, indexD, indexB);
+    }
+    
+    addFinalIndices(j) {
+        let points = this.vertices.length / 3;
+        let indexC = j - 1;
+        let indexD = j;
+        let indexB = points - this.stacks - 1 + j;
+        let indexA = indexB - 1;
+        this.indices.push(indexA, indexC, indexD, indexA, indexD, indexB);
+    }
+
 
     updateBuffers(complexity) {
     }
 }
-*/
--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -17,46 +17,72 @@ export class MyPrism extends CGFobject {
 	}
 	
 	initBuffers() {
-        
+        this.initializeArrays();
+        this.calculateVerticesIndicesAndNormals();
+        this.setPrimitiveTypeAndInitializeGLBuffers();
+    }
+    
+    initializeArrays() {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
-
+    }
+    
+    calculateVerticesIndicesAndNormals() {
         let index = 0;
         let increment = 2 * Math.PI / this.slices;
+    
         for (let i = 0 ; i < this.slices ; i++) {
-
-            let x1 = Math.cos(i*increment);
-            let y1 = Math.sin(i*increment);
-            let x2 = Math.cos((i+1)*increment);
-            let y2 = Math.sin((i+1)*increment);
-            
-            let incrementZ = 1 / this.stacks
+            let {x1, y1, x2, y2} = this.calculateXY(i, increment);
+            let incrementZ = 1 / this.stacks;
+    
             for (let j = 0 ; j < this.stacks ; j++) {
-
-                let x = Math.cos((i+0.5)* increment);
-                let y = Math.sin((i+0.5)*increment);
-                let size = Math.sqrt(x*x + y*y);
-
-                this.vertices.push(x1, y1, incrementZ * j); 
-                this.vertices.push(x2, y2, incrementZ * j);
-                this.vertices.push(x1, y1, incrementZ * (j + 1)); 
-                this.vertices.push(x2, y2, incrementZ * (j + 1));
-
-                this.indices.push(index+2, index, index+1);
-                this.indices.push(index+1, index+3, index+2);
-
-                this.normals.push(x/size, y/size, 0, x/size);
-                this.normals.push(y/size, 0, x/size, y/size);
-                this.normals.push(0, x/size, y/size, 0);
-                
-                index+=4;
+                let {x, y, size} = this.calculateXYS(i, increment);
+                this.addVertices(x1, y1, x2, y2, incrementZ, j);
+                this.addIndices(index);
+                this.addNormals(x, y, size);
+                index += 4;
             }
         }
-		this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();
+    }
     
-	}
+    calculateXY(i, increment) {
+        let x1 = Math.cos(i * increment);
+        let y1 = Math.sin(i * increment);
+        let x2 = Math.cos((i + 1) * increment);
+        let y2 = Math.sin((i + 1) * increment);
+        return {x1, y1, x2, y2};
+    }
+    
+    calculateXYS(i, increment) {
+        let x = Math.cos((i + 0.5) * increment);
+        let y = Math.sin((i + 0.5) * increment);
+        let size = Math.sqrt(x * x + y * y);
+        return {x, y, size};
+    }
+    
+    addVertices(x1, y1, x2, y2, incrementZ, j) {
+        this.vertices.push(x1, y1, incrementZ * j); 
+        this.vertices.push(x2, y2, incrementZ * j);
+        this.vertices.push(x1, y1, incrementZ * (j + 1)); 
+        this.vertices.push(x2, y2, incrementZ * (j + 1));
+    }
+    
+    addIndices(index) {
+        this.indices.push(index + 2, index, index + 1);
+        this.indices.push(index + 1, index + 3, index + 2);
+    }
+    
+    addNormals(x, y, size) {
+        this.normals.push(x / size, y / size, 0, x / size);
+        this.normals.push(y / size, 0, x / size, y / size);
+        this.normals.push(0, x / size, y / size, 0);
+    }
+    
+    setPrimitiveTypeAndInitializeGLBuffers() {
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
 }
 
 
